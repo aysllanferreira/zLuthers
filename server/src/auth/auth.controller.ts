2c7a110b38caa6eafffce3071fe752a89e1924df
“Aysllan <aysllan.ferreiraks@gmail.com>
2023-05-08 19:26:39 -0300

    :sparkles: feat: Signup done

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('sign-up')
+  async signUp(
+    @Body()
+    signUpRequest: {
+      nickname: string;
+      email: string;
+      password: string;
+    },
+  ) {
+    try {
+      return await this.authService.signUp(signUpRequest);
+    } catch (err) {
+      throw new BadRequestException(err.message);
+    }
+  }
+}
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,4 +1,42 @@
+import { AuthConfig } from './auth.config';
 import { Injectable } from '@nestjs/common';
+import {
+  AuthenticationDetails,
+  CognitoUser,
+  CognitoUserPool,
+  CognitoUserAttribute,
+} from 'amazon-cognito-identity-js';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  private userPool: CognitoUserPool;
+  private sessionUserAttributes: any;
+  constructor(private readonly authConfig: AuthConfig) {
+    this.userPool = new CognitoUserPool({
+      UserPoolId: this.authConfig.userPoolId,
+      ClientId: this.authConfig.clientId,
+    });
+  }
+
+  async signUp(signUpRequest: {
+    nickname: string;
+    email: string;
+    password: string;
+  }) {
+    const { nickname, email, password } = signUpRequest;
+    return new Promise((resolve, reject) => {
+      return this.userPool.signUp(
+        email,
+        password,
+        [new CognitoUserAttribute({ Name: 'nickname', Value: nickname })],
+        null,
+        (err, result) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(result);
+        },
+      );
+    });
+  }
+}
